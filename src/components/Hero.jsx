773d8c25bd328b86f34f4278a905065f6f02ce9e
unknown <z5532071@ad.unsw.edu.au>
2025-05-28 19:29:27 +0800

    start hero component and finish css setting

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    //dvh is used to ensure the hero section takes the full viewport height
+    <div className='relative  h-dvh w-screen overflow-x-hidden'>
+        <div id="video-frame" className='relative  h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
