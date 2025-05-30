06a16c5d03ecb020f2fcf90f85e25e3810ac16a7
unknown <z5532071@ad.unsw.edu.au>
2025-05-30 13:40:45 +0800

    finish story section image animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
 import Features from './components/Features'
+import Story from './components/Story'
 
 const App = () => {
   return (
@@ -15,6 +16,8 @@ const App = () => {
       <About/>
 
       <Features/>
+
+      <Story/>
       
     </main>
   )
